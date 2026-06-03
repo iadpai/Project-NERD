2ad1ba4eba83810cdd8306b25e6740076ade7ed5
Tony Bourke <tonybourke@users.noreply.github.com>
2026-06-03 16:50:33 -0700

    Update enable_debian_13_https.sh

--- Autobox/enable_debian_13_https.sh
+++ Autobox/enable_debian_13_https.sh
@@ -91,7 +91,7 @@ EOF
 
 sudo apt update
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
 sudo systemctl enable docker
